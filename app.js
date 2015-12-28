ebebaa9c0a012d5e20633123a475b5d5cb7c8ed1
pratimary <pratima.mary@gmail.com>
2015-12-27 20:20:14 -0600

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    app= express(),
+    server= require('http').createServer(app),
+    io= require('socket.io').listen(server),
+    path = require('path');
+
+var users=[];
+    server.listen(3000);
+
+    app.get('/',function(req,res)
+    {
+        res.sendFile(__dirname + '/index.html' );
+    });
+//app.use(express.static(path.join(__dirname, '/html')));
+//app.use(express.static(__dirname + '/html'));
+io.sockets.on('connection',function(socket)
+{
+    console.log("new user connected");
+    socket.on('new user',function(data)
+    {
+        socket.context=data;
+      users.push(socket.context);
+       // console.log(data);
+       io.sockets.emit('all users',users);
+    });
+    socket.on('send message',function(data){
+        io.sockets.emit('new message',data);
+    });
+
+    socket.on('send coordinates',function(data)
+    {
+        socket.broadcast.emit('draw',data);
+       //console.log(data);
+    });
+});
